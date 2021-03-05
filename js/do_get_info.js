1d4a37139e1d0828ea695952e5157e6677943823
fituef <56488063+fituef@users.noreply.github.com>
2021-03-05 15:25:58 +0700

    upadte
    upadte

--- js/do_get_info.js
+++ js/do_get_info.js
@@ -76,7 +76,7 @@ function doComplete() {
 }
 
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbw_XGQmbp8hRLvLWcdqO3il7xY8gPuGftavBU7YGX_exc8TzPo/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyCoeni46IJO1cHgkYamQFqfqhpT-JJHwtOBsA2GFHxU6GDyQuseGC0BN0Y0iPpCfE1eA/exec';
 
 const loading = document.querySelector('.js-loading');
 const successMessage = document.querySelector('.js-success-message');
